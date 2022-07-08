11358a0815baf690df8c8fe852abac18e067526c
iPsxp <100557866+iPsxp@users.noreply.github.com>
2022-07-08 19:46:04 +0800

    Update Select_payment_method.js

--- pxx/js/Select_payment_method.js
+++ pxx/js/Select_payment_method.js
@@ -1,3 +1,9 @@
+// 页面加载完后执行
 $(document).ready(function(){
+    // 选择支付宝支付宝
     $("#main > div > div.jkEfnqgK > div:nth-child(2) > div > div").click();
+
+    // 点击立即支付
+    $("#main > div > div:nth-child(8) > div.x7SLg_Nx").click();
+    
 });
